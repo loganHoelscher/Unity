76683db14c85b1274de056f32223d362168547e4
Logan Hoelscher <60831553+loganHoelscher@users.noreply.github.com>
2020-06-16 21:44:06 -0500

    Add files via upload

---
+++ PredatorMissile/missileControl.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class missileControl : MonoBehaviour
+{
+    public GameObject boom;
+    public GameObject scorch;
+
+    public float speed = 150.0f;
+    public float blastRadius = 5f;
+    public float blastForce = 100f;
+
+    public Camera missileCamThird;
+    public Camera missileCamFirst;
+    public Camera sceneCam;
+
+
+    bool hasBoomed = false;
+    bool isScorched = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        missileCamThird.enabled = true;
+        missileCamFirst.enabled = false;
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(speed * Time.deltaTime, 0f, 0f);
+
+        if (transform.position.y < 600)
+        {
+            speed += 15;
+            missileCamThird.enabled = false;
+            missileCamFirst.enabled = true;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Plane" || collision.gameObject.name == "white")
+        {
+            if (!hasBoomed && !isScorched)
+            {
+                missileCamThird.enabled = false;
+                missileCamFirst.enabled = true;
+                sceneCam.enabled = true;
+                Explode();
+                hasBoomed = true;
+                isScorched = true;
+                Destroy(this.gameObject);
+            }
+
+
+        }
+    }
+    private void Explode()
+    {
+        Instantiate(boom, transform.position, transform.rotation);
+        Instantiate(scorch, transform.position, transform.rotation);
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, blastRadius);
+
+        foreach(Collider nearbyObject in colliders)
+        {
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+
+            if(rb != null)
+            {
+                rb.AddExplosionForce(blastForce, transform.position, blastRadius);
+            }
+        }
+    }
+}
+
+   
---
+++ PredatorMissile/mouseLook.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseLook : MonoBehaviour
+{
+
+    public float mouseSens = 3.0f;
+    float xRotation = 0f;
+    float yRotation = 0f;
+    public Transform player;
+    // Use this for initialization
+    void Start()
+    {
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSens;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSens;
+
+        xRotation -= mouseY;
+        yRotation -= mouseX;
+        xRotation = Mathf.Clamp(xRotation, -20f, 90f);
+        yRotation = Mathf.Clamp(xRotation, -20f, 90f);
+       // transform.localRotation = Quaternion.Euler(0f, xRotation, 0f);
+       // transform.localRotation = Quaternion.Euler(yRotation, 0f, 0f);
+        player.Rotate(Vector3.up * mouseX);
+        player.Rotate(Vector3.forward * mouseY);
+
+
+
+    }
+}
